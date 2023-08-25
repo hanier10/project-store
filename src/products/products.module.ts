8d172bce6c2eadf2039da20007b5617928abbaf2
Hanier Morales <hsmp16@gmail.com>
2023-08-25 16:43:07 -0600

    Update products.module.ts

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from './entities/product.entity';
 import { ProductsController } from './controllers/products.controller';
 import { ProductsService } from './services/products.service';
+import { ProductImage } from './entities/product-image.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product])],
+  imports: [TypeOrmModule.forFeature([Product, ProductImage])],
   controllers: [ProductsController],
   providers: [ProductsService],
 })
