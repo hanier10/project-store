fc07e57080c3e089f6ded27a10cef302e4220630
Hanier Morales <hsmp16@gmail.com>
2023-08-25 16:56:45 -0600

    Delete files directory

--- files/files.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { FilesController } from './controllers/files.controller';
-import { FilesService } from './services/files.service';
-
-@Module({
-  controllers: [FilesController],
-  providers: [FilesService],
-})
-export class FilesModule {}
