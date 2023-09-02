bf04433f7e19e42cad1e043f027003d2fd8115ad
Hanier <hanier@MacBook-Pro-de-Hanier.local>
2023-09-02 11:21:24 -0600

    agregando estructura para subida de archivos

--- src/files/controllers/files.controller.ts
+++ src/files/controllers/files.controller.ts
@@ -1,12 +1,40 @@
-import { Controller, Post } from '@nestjs/common';
+import {
+  BadRequestException,
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { FilesService } from '../services/files.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { fileFilter } from 'src/helpers/fileFilter.helper';
+import { diskStorage } from 'multer';
+import { fileNamer } from 'src/helpers/fileNamer.helper';
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
 
-  @Post('product')
-  UploadImage() {
-    return 'Hola Mundo';
+  @Post('upload')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      //Llamamos al fileFilter de Multer y le asignamos nuestro helper
+      fileFilter: fileFilter,
+
+      //Definimos en donde se va a guardar el archivo y lo renombramos
+      storage: diskStorage({
+        destination: './static/products/',
+        filename: fileNamer,
+      }),
+    }),
+  )
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    if (!file) {
+      throw new BadRequestException('Asegurese que el archivo es una imagen');
+    }
+
+    return {
+      fileName: file.filename,
+    };
   }
 }
